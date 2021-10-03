dc5eae1bfbf22205204a485950c22bf59ef6c04d
Joel Humberto Gomez Paredes <elmaildeldezkareid@gmail.com>
2021-10-03 00:52:11 -0500

    chore(react-hooks): setup tools

---
+++ packages/react-hooks/README.md
@@ -0,0 +1,25 @@
+# React hooks
+
+This is just a collection of common functionality I use in my projects
+
+## Before to start
+You need the next packages installed:
+* react: ^16.0.0 || ^17.0.0
+
+## Getting started
+
+To install `@dezkareid/react-hooks` in your project, run next command:
+
+```bash
+npm install -S @dezkareid/react-hooks
+```
+
+or
+
+```bash
+yarn add @dezkareid/react-hooks
+```
+
+## List of hooks
+
+* [useLocalStorage](src/useLocalStorage/README.md)
---
+++ packages/react-hooks/rollup.config.js
@@ -0,0 +1,50 @@
+import { babel } from '@rollup/plugin-babel';
+import cleaner from 'rollup-plugin-cleaner';
+import commonjs from '@rollup/plugin-commonjs';
+import resolve from "@rollup/plugin-node-resolve";
+import { terser } from "rollup-plugin-terser";
+import pkg from './package.json';
+
+const config = {
+  input: ['src/index.js', 'src/useLocalStorage'],
+  output: [
+    {
+      dir: 'dist/es',
+      format: 'es'
+    },
+    {
+      dir: 'dist/cjs',
+      format: 'cjs'
+    }
+  ],
+  external: Object.keys(pkg.peerDependencies),
+  plugins: [
+    cleaner({
+      targets: [
+        './dist/'
+      ]
+    }),
+    resolve(),
+    commonjs(),
+    babel({
+      babelrc: false,
+      exclude: ['**/node_modules/**'],
+      presets: [
+        [
+          '@babel/preset-env',
+          {
+            modules: false,
+          },
+        ],
+      ],
+      plugins: [
+        '@babel/plugin-proposal-export-default-from',
+        '@babel/plugin-transform-runtime'
+      ],
+      babelHelpers: 'runtime',
+    }),
+    terser()
+  ]
+};
+
+export default config;
