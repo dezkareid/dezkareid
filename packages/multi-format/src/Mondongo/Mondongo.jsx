3ae05e571f0cc016d17803e46ecca6c65400802b
Joel Humberto Gomez Paredes <elmaildeldezkareid@gmail.com>
2024-10-26 14:31:59 -0500

    fix: styles

--- packages/multi-format/src/Mondongo/Mondongo.jsx
+++ packages/multi-format/src/Mondongo/Mondongo.jsx
@@ -1,3 +1,15 @@
+const stylesContainer = {
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  height: '100vh',
+  width: '100vw',
+};
+
+const stylesText = {
+  fontSize: '6rem',
+};
+
 export const Mondongo = () => {
-  return <div>Mondongo</div>;
+  return <div style={stylesContainer}><span style={stylesText}>Mondongo</span></div>;
 };
