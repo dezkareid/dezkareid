979c889661012d06bccee33cf97dacb3c6bb5be6
Joel Humberto Gomez Paredes <elmaildeldezkareid@gmail.com>
2026-03-19 22:57:29 -0600

    refactor: return compile strategy

--- apps/main-website/astro.config.mjs
+++ apps/main-website/astro.config.mjs
@@ -6,8 +6,7 @@ import sitemap from '@astrojs/sitemap';
 export default defineConfig({
   output: 'server',
   adapter: cloudflare({
-    // TODO: change this to 'compile' when we have more than 2 images
-    imageService: 'passthrough',
+    imageService: 'compile',
   }),
   site: 'https://dezkareid.dev',
   integrations: [sitemap()],
