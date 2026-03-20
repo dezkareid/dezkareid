72f53c80698933bf71f80683488d89cd71e2a876
Joel Humberto Gomez Paredes <elmaildeldezkareid@gmail.com>
2026-03-19 22:39:44 -0600

    refactor: change image strategy

--- apps/main-website/astro.config.mjs
+++ apps/main-website/astro.config.mjs
@@ -5,7 +5,10 @@ import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   output: 'server',
-  adapter: cloudflare(),
+  adapter: cloudflare({
+    // TODO: change this to 'compile' when we have more than 2 images
+    imageService: 'passthrough',
+  }),
   site: 'https://dezkareid.dev',
   integrations: [sitemap()],
 });
